a8cf68bb6e124a184570c9e83340b393420a662d
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-02-08 12:00:27 -0500

    TESTS: add game tests

--- lib/game.rb
+++ lib/game.rb
@@ -2,9 +2,10 @@
 require_relative './board.rb'
 
 class Game
-
-  def initialize
-    @board = Board.new
+  attr_accessor :board
+  
+  def initialize(board = Board.new)
+    @board = board
   end
 
   def turn
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,14 @@
-require_relative './lib/game'
+require_relative '../lib/game'
+require_relative '../lib/board'
 
 describe Game do
-  
+  let(:game) { Game.new(instance_double_board) }
+  let(:instance_double_board) { instance_double(Board) }
+
+  describe '#initialize' do
+    it 'initializes an empty board' do
+      allow(game).to receive(:board).and_return(Array.new(9, ' '))
+      expect(game.board).to eq(Array.new(9,' '))
+    end
+  end
 end
